f79536f75f3469d3503355880a34cf9334cbc102
Kelvin David <kdroomo416@gmail.com>
2020-06-04 21:16:36 +0000

    Tests #3

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -42,16 +42,16 @@ describe('Get Weather', function() {
       }
     };
 
-    //const request = function( obj, callback ){
-      //callback("error", null, null);
-    //};
+    const request = function( obj, callback ){
+      callback("error", null, null);
+    };
 
-    //apiv1.__set__("request", request);
+    apiv1.__set__("request", request);
 
     apiv1.getWeather(reqMock, resMock);
 
     assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
-    //assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
+    assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
   });
 
   it('with incomplete city name', function() {
@@ -61,16 +61,16 @@ describe('Get Weather', function() {
       }
     };
 
-//    const request = function( obj, callback ){
-//      callback(null, null, {});
-//    };
+    const request = function( obj, callback ){
+      callback(null, null, {});
+    };
 
-    //apiv1.__set__("request", request);
+    apiv1.__set__("request", request);
 
     apiv1.getWeather(reqMock, resMock);
 
     assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
-    //assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
+    assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
   });
 
   it('with valid city name', function() {
@@ -80,29 +80,29 @@ describe('Get Weather', function() {
       }
     };
 
-//    const body = {
-//      cod: 200,
-//      name: 'Hamilton',
-//      weather: [
-//        {
-//          main: 'cold'
-//        }
-//      ],
-//      main: {
-//        temp: 78
-//      }
-//    };
+    const body = {
+      cod: 200,
+      name: 'Hamilton',
+      weather: [
+        {
+          main: 'cold'
+        }
+      ],
+      main: {
+        temp: 78
+      }
+    };
 
-//    const request = function( obj, callback ){
-//      callback(null, null, body);
-//    };
+    const request = function( obj, callback ){
+      callback(null, null, body);
+    };
 
-//    apiv1.__set__("request", request);
+    apiv1.__set__("request", request);
 
     apiv1.getWeather(reqMock, resMock);
 
     assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
-    //assert(resMock.send.lastCall.args[0].city === 'Hamilton', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
-    //assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
+    assert(resMock.send.lastCall.args[0].city === 'Hamilton', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+    assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 C', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
   });
 });
